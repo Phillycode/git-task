88790d30ff43990853ed2c7996293d6a5c885b4c
Philip Bergh <pjbergh@gmail.com>
2025-06-16 20:36:39 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
+# Practice file to be pushed to my github repository
 print("Git is Awesome!")
+
+# Update: after attempting to push this file to my repository, I got an error
+# and after further research, I found that due to the README file that was in
+# the repository already, I could not push the file directly without using
+# the "force" command. Therefore, I decided to attempt the pull command and
+# merged the repository to my local git repository. I then pushed it again
+# and it worked! I modified this file and pushed it again, for some extra
+# practice.
